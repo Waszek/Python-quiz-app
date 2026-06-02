cd3e31f73675077b53f1632f19716f5cddc965bb
Krzysztof W <waszur20@gmail.com>
2026-06-02 21:14:54 +0200

    added class QuizGame

--- demo.py
+++ demo.py
@@ -9,6 +9,43 @@ class Question:
     def check_answer(self, user_answer):
         return user_answer == self.correct_answer
 
+class QuizGame:
+    def __init__(self, player_name, file_path):
+        self.player_name = player_name
+        self.file_path = file_path
+        self.questions = []
+        self.points = 0
+
+        self.load_questions()
+
+    def load_questions(self):
+        with open(self.file_path) as file:
+            data = json.load(file)
+            
+            for element in data:
+                question_obj = Question(
+                    element["question"],
+                    element["options"],
+                    element["correct"]
+                )
+                self.questions.append(question_obj)
+        
+
+    def start(self):
+        for question in self.questions:
+            print(question.text)
+            options = "\n".join(f"{k}: {v}" for k,v in question.option.items())
+            print(options)
+            user_answer = input("Your answer: ")
+
+            if question.check_answer(user_answer):
+                print("Correct answer!")
+                self.points += 1
+            else:
+                print("Wrong answer!")
+
+        print(f"{self.player_name}, your score is {self.points}")
+        
 
 def welcome():
     first_name = input('What is your first name? ')
@@ -35,41 +72,10 @@ def welcome():
     return first_name, age
 
 
-
-def quiz(first_name):
-    with open('questions.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-
-    question_objects = []
-
-    points = 0
-
-    for element in data:
-       question_obj = Question(
-           element["question"],
-           element["options"],
-           element["correct"]
-       )
-       question_objects.append(question_obj)
-
-    for question in question_objects:
-        print(question.text)
-        options = "\n".join(f"{k}: {v}" for k,v in question.option.items())
-        print(options)
-        answer = input('Your answer: ')
-
-        if question.check_answer(answer):
-                print('Correct answer!')
-                points += 1
-        else:
-                print('Wrong answer!')
-    
-
-    print(f'{first_name}, Your score is: {points}')
-
 def main():
     received_first_name, received_age = welcome()
-    quiz(received_first_name)
+    game = QuizGame(received_first_name, file_path='questions.json')
+    game.start()
 
 if __name__ == "__main__":
     main()
